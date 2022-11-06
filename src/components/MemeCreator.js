f979c31394e9a14083a831ea1b3a71be73e5b26f
Chris <chrishethertonweb@gmail.com>
2022-11-06 19:28:08 +0100

    retouched css

--- src/components/MemeCreator.js
+++ src/components/MemeCreator.js
@@ -4,10 +4,10 @@ function MemeCreator() {
     return ( 
         <div className="meme-box">
             <section className="meme-box__top">
+                <label htmlFor=""></label>
+                <input type="text" placeholder='Top Text' />
                 <label htmlFor=""/>
-                <input type="text" />
-                <label htmlFor=""/>
-                <input type="text" />
+                <input type="text" placeholder='Bottom Text'/>
             </section>
             <section className="meme-box__bottom">
                 <button className='background-colour '>Get a new Image <GrImage/></button>
