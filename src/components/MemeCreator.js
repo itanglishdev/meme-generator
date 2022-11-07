b4caa59afcdc6c586544b727a4a51f3d31401736
Chris <chrishethertonweb@gmail.com>
2022-11-07 20:26:52 +0100

    added dynamic and css to MemeCreator

--- src/components/MemeCreator.js
+++ src/components/MemeCreator.js
@@ -13,10 +13,10 @@ function MemeCreator() {
     const [meme, setMeme] = useState({
         topText:'',
         bottomText: '',
-        randomImage: ''
+        randomImage: 'http://i.imgflip.com/1bij.jpg'
     })
 
-    const [allMemeImages, setAllMemeImages] = useState(data)
+    // const [allMemeImages, setAllMemeImages] = useState(data)
 
     function changeMemeHandler() {
 
@@ -30,14 +30,24 @@ function MemeCreator() {
         <div className="meme-box">
             <section className="meme-box__top">
                 <label htmlFor=""></label>
-                <input type="text" placeholder='Top Text' />
+                <input 
+                type="text" 
+                placeholder='Top Text' 
+                />
                 <label htmlFor=""/>
-                <input type="text" placeholder='Bottom Text'/>
+                <input 
+                type="text" 
+                placeholder='Bottom Text'
+                />
             </section>
             <section className="meme-box__bottom">
                 <button onClick={changeMemeHandler} className='background-colour '>Get a new Image 🖼</button>
             </section>
-            <img className='meme-box__image' src={meme.randomImage } alt="" />
+            <div className='meme-box__container flex'>
+                <img className='meme-box__image' src={meme.randomImage } alt="" />
+                <h2 className='meme-text top' >one does not simply</h2>
+                <h2 className='meme-text bottom' >walk into mordor</h2>
+            </div>
         </div>
      );
 }
